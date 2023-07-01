80a3877d633b9a92ce2a59c08bace2b5437e5bed
Prateek Rustagi <prateekrustagi2001@gmail.com>
2023-07-01 15:31:30 +0530

    added few things

--- Client/src/components/auth/login.js
+++ Client/src/components/auth/login.js
@@ -1,8 +1,14 @@
 const login = () => {
 
     return (
-      <div>
-        
+      <div className="login-out">
+        <div className="login-heading">
+            Sign in via phone number 
+        </div>
+
+        <div className="input-phone-number">
+
+        </div>
       </div>
     );
   };
