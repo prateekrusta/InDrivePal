9de646943a1f623144aa03cdc6b3c8d7cde79e0d
Prateek Rustagi <prateekrustagi2001@gmail.com>
2023-07-01 16:41:01 +0530

    asdfgh

--- Client/src/components/auth/login.js
+++ Client/src/components/auth/login.js
@@ -1,4 +1,9 @@
-const login = () => {
+import Grid from "@material-ui/core/Grid"
+import ReactDOM from "react-dom";
+import indianflag from '../../assets/logos/indiaflag.webp';
+import '../../assets/css/login.css';
+
+const Login = () => {
 
     return (
       <div className="login-out">
@@ -7,10 +12,30 @@ const login = () => {
         </div>
 
         <div className="input-phone-number">
+            <Grid container spacing={2}>
+                <Grid item xs={4}>
+                    <div className="input-phone-number-left">
+                        <img src={indianflag} className="flag"></img>
+                    </div>
+                </Grid>
 
+                <Grid item xs={8}>
+                    <div className="input-phone-number-right">
+                        <span> +91 </span>
+                        <input type="tel" placeholder="Enter your phone number"></input>
+                    </div>
+                </Grid>
+                
+            </Grid>
+        </div>
+
+        <div className="disclaimer">
+            <p>
+                By tapping "Next" 
+            </p>
         </div>
       </div>
     );
   };
   
-  export default login;
+  export default Login;
