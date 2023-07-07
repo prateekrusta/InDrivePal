c3fe335c846650262b1582e36b16fb46af254052
Prateek Rustagi <prateekrustagi2001@gmail.com>
2023-07-08 04:46:19 +0530

    added more changes to book ride

--- Client/src/components/ride/book-ride.js
+++ Client/src/components/ride/book-ride.js
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 import Grid from "@material-ui/core/Grid"
 import ReactDOM from "react-dom";
 import Navbar from '../ride/navbar';
-import Googlemapsimg from '../../assets/images/GoogleMapTA.png'; 
+import Switch from '@mui/material/Switch';
 import '../../assets/css/book-ride.css';
 import '../../assets/css/buttons.css';
 
@@ -13,6 +13,9 @@ import ElectricRickshawIcon from '../../assets/logos/47.png';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
 import MessageIcon from '@mui/icons-material/Message';
+import InfoIcon from '@mui/icons-material/Info';
+import AccessibleIcon from '@mui/icons-material/Accessible';
+import AirportShuttleIcon from '@mui/icons-material/AirportShuttle';
 
 const Bookride = () => {
 
@@ -28,26 +31,26 @@ const Bookride = () => {
           <div className='input-form'>
             <div className='vehicle-icons'>
               <Grid container spacing={1}>
-                <Grid item xs={3} className='vehicle-container'>
-                  <img src={LocalACTaxiIcon} className='ride-logos' />
+                <Grid item xs={3} className='vehicle-container vcc-1'>
+                  <img src={LocalACTaxiIcon} className='ride-logos' /><InfoIcon className='info-icons info-icons-1' />
                   <br />
                   <label>Ride AC</label>
                 </Grid>
 
-                <Grid item xs={3} className='vehicle-container'>
-                  <img src={LocalTaxiIcon} className='ride-logos ride-logos123' />  
+                <Grid item xs={3} className='vehicle-container vcc-2'>
+                  <img src={LocalTaxiIcon} className='ride-logos ride-logos123' /><InfoIcon className='info-icons info-icons-2' />
                   <br />
                   <label>Ride</label>
                 </Grid>
 
-                <Grid item xs={3} className='vehicle-container'>
-                  <img src={ElectricRickshawIcon} className='ride-logos' />
+                <Grid item xs={3} className='vehicle-container vcc-3'>
+                  <img src={ElectricRickshawIcon} className='ride-logos' /><InfoIcon className='info-icons info-icons-3' />
                   <br />
                   <label>Auto</label>
                 </Grid>
 
-                <Grid item xs={3} className='vehicle-container'>
-                  <img src={TwoWheelerIcon} className='ride-logos' />
+                <Grid item xs={3} className='vehicle-container vcc-4'>
+                  <img src={TwoWheelerIcon} className='ride-logos' /><InfoIcon className='info-icons info-icons-4' />
                   <br />
                   <label>Moto</label>
                 </Grid>
@@ -83,6 +86,22 @@ const Bookride = () => {
                 </Grid>
                 <Grid item xs={10}>
                   <input type="text" placeholder='Options and Comments'></input>
+                </Grid>
+
+                <Grid item xs={2}>
+                  <AirportShuttleIcon className='book-ride-icons'  />
+                </Grid>
+                <Grid item xs={10}>
+                  <label>More than 4 passengers</label>
+                  <Switch className='switcher' />
+                </Grid>
+
+                <Grid item xs={2}>
+                  <AccessibleIcon className='book-ride-icons'  />
+                </Grid>
+                <Grid item xs={10}>
+                  <label>Wheelchair friendly rides</label>
+                  <Switch className='switcher' />
                 </Grid>
               </Grid>
             </div>
