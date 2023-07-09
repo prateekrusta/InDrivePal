90b3c4c1e5df2d88d946872bb166356e507288ce
Prateek Rustagi <prateekrustagi2001@gmail.com>
2023-07-09 07:49:19 +0530

    added chat page

--- Client/src/components/chat/ChatMsg.js
+++ Client/src/components/chat/ChatMsg.js
@@ -4,10 +4,22 @@ import { TextInput } from "./TextInput.js";
 import { MessageLeft, MessageRight } from "./Message";
 import Chatheader from './Chatheader.js';
 import '../../assets/css/chat.css'
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 
+export default function ChatMsg() {
 
-export default function App() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className="chat-outside">
     <Chatheader />
@@ -16,19 +28,35 @@ export default function App() {
 
         <div id="style-1" className="messagesBody">
 
+        <a id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}>
+
+          <MessageLeft
+            message="Hey Hey Hey Hey HeyHey Hey Hey Hey HeyHey Hey Hey Hey HeyHey Hey Hey Hey Hey"
+            photoURL=""
+            displayName="Prateek"
+            avatarDisp={true}
+          />
+          
+          </a>
+
+          <a id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}>
+
           <MessageLeft
             message="Hey"
             photoURL=""
             displayName="Prateek"
             avatarDisp={true}
           />
-
-          <MessageLeft
-            message="Hi"
-            photoURL=""
-            displayName="Prateek"
-            avatarDisp={false}
-          />
+          
+          </a>
 
           <MessageRight
             message="Hey"
@@ -162,6 +190,23 @@ export default function App() {
       </div>
     </div>
     <TextInput />
+
+    <Menu
+        className="menu-chat"
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Copy</MenuItem>
+        <MenuItem onClick={handleClose}>Delete</MenuItem>
+        <MenuItem onClick={handleClose}>Text To Audio</MenuItem>
+        <MenuItem onClick={handleClose}>Report</MenuItem>
+      </Menu>
+
     </div>
   );
 }
