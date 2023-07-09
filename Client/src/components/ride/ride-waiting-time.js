490b4c46b6222274da56ab601d3ac398162515c5
Prateek Rustagi <prateekrustagi2001@gmail.com>
2023-07-09 22:21:43 +0530

    added styles

--- Client/src/components/ride/ride-waiting-time.js
+++ Client/src/components/ride/ride-waiting-time.js
@@ -1,11 +1,13 @@
+import '../../assets/css/book-ride.css';
+
 const WaitingTime = () => {
     return(
-      <div style={{display:'flex', flexDirection:'row', marginLeft:'1vw', justifyContent:'space-between'}}>
+      <div className='waiting-outer' style={{display:'flex', flexDirection:'row', marginLeft:'1vw', justifyContent:'space-between'}}>
        <div style={{display:'flex', flexDirection:'column', marginLeft:'1vw'}}>
         <h2 style={{textAlign:'left',  fontSize:'30px', color:'black'}}>ETA</h2>
         <h3 style={{fontWeight:100, color: 'grey', textAlign:'left', marginTop:-15}}>Please be on time!</h3> 
        </div>
-       <h1 style={{textAlign:'right', marginTop:'1.5vh', marginRight:'1vw'}}>6mins</h1>
+       <h1 style={{textAlign:'right', marginTop:'2.5vh'}}>6mins</h1>
       </div>
 )}
 
